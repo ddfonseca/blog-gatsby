4d636c0bf1e8b81b0f894440e40f6c94964bf95c
ddfonseca <olfdavid@gmail.com>
2021-09-23 11:58:26 -0300

    change gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     // needs to be the first to work with gatsby-remark-images
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `uploads`,
-        path: `${__dirname}/static/assets/img`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `uploads`,
+    //     path: `${__dirname}/static/assets/img`,
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
