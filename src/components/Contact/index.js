d034e064cfbbb7968f64ecbcda3eddcf99c8404e
ddfonseca <olfdavid@gmail.com>
2021-09-30 15:42:16 -0300

    about me updated

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Layout from '../Layout'
 import Header from '../Header'
-// import * as S from './styled'
+import * as S from './styled'
 
 const Contact = () => {
   return (
     <Layout>
       <Header />
-      Olá 👋!
+      <p>
+        Olá 👋! Tudo bem? Meu nome é David (lê-se Davi), tenho 30 anos e sou
+        apaixonado por programação!
+      </p>
+      <br />
+      <p>Você pode me encontrar em:</p>
+      <br />
+      <S.Links>
+        <S.LinkItem>
+          <S.Link href="https://twitter.com/ddfonseca6" target="__blank">
+            <S.Twitter /> @ddfonseca6
+          </S.Link>
+        </S.LinkItem>
+        <S.LinkItem>
+          <S.Link href="mailto:olfdavid@gmail.com">
+            <S.Email /> olfdavid@gmail.com
+          </S.Link>
+        </S.LinkItem>
+        <S.LinkItem>
+          <S.Link
+            href="https://www.linkedin.com/in/david-fonseca-16746ab6/"
+            target="__blank"
+          >
+            <S.Linkedin /> Perfil no Linkedin
+          </S.Link>
+        </S.LinkItem>
+      </S.Links>
     </Layout>
   )
 }
--- src/components/Contact/styled.js
+++ src/components/Contact/styled.js
@@ -1,4 +1,38 @@
-import styled from 'styled-components'
-import { Twitter } from '@styled-icons/boxicons-logos/Twitter'
+import styled, { css } from 'styled-components'
+import { Twitter as T } from '@styled-icons/boxicons-logos/Twitter'
+import { Email as E } from '@styled-icons/material-rounded/Email'
+import { LinkedinSquare as L } from '@styled-icons/boxicons-logos/LinkedinSquare'
 
 export const ContactWrapper = styled.div``
+
+const base = css`
+  width: 1.5rem;
+`
+
+export const Links = styled.ul`
+  list-style: none;
+`
+
+export const LinkItem = styled.li`
+  &:hover {
+    color: var(--link);
+    cursor: pointer;
+  }
+`
+
+export const Link = styled.a`
+  color: inherit;
+  text-decoration: none;
+`
+
+export const Twitter = styled(T)`
+  ${base}
+`
+
+export const Email = styled(E)`
+  ${base}
+`
+
+export const Linkedin = styled(L)`
+  ${base}
+`
